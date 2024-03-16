e9e4b34d4ef41568b64bb3971e517dec6e5da7ab
Владислав <pahnetstarushkami@bk.ru>
2024-03-17 00:01:53 +0300

    character base

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -1,12 +1,25 @@
 extends CharacterBody2D
 
 @export var speed: int = 35
+@onready var animations = $AnimationPlayer
 
 func getInput():
 	var moveDirection = Input.get_vector("left", "right", "up", "down")
 	velocity = moveDirection * speed
 	
-
+func updateAnimation():
+	if velocity.length() == 0:
+		animations.stop()
+	else: 
+		var direction = "Down"
+		animations.play("walk" + direction)
+		
+		
 func _physics_process(delta):
+	updateAnimation()
 	getInput()
 	move_and_slide()
+	
+
+
+
